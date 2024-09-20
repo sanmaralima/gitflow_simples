42c73ecfb57f025216a0ea2e61d9a42991e21767
unknown <sanmara06@gmail.com>
2024-09-20 14:35:42 -0300

    Adição de model e controller

---
+++ src/main/java/pji/ltp/demo/model/Usuaria.java
@@ -0,0 +1,10 @@
+package pji.ltp.demo.model;
+
+public class Usuaria {
+    private String idUsuaria;
+    private String nome;
+    private String cpf;
+    private int idade;
+    private boolean assinante;
+
+}
